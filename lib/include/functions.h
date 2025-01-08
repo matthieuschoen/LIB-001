a14b2971afa77530821bcefb7d8ae6c3d0a70e91
Matthieu Schoen <matthieu.schoen@epitech.eu>
2025-01-08 02:05:55 +0100

    🩺 add my_isdigit with coding style check

---
+++ lib/include/functions.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** LIB-001
+** File description:
+** functions
+*/
+
+#ifndef FUNCTIONS_H_
+    #define FUNCTIONS_H_
+
+int my_isdigit(char c);
+
+#endif /* !FUNCTIONS_H_ */
---
+++ lib/my_isdigit.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** LIB-001
+** File description:
+** my_isdigit
+*/
+
+#include "functions.h"
+
+int my_isdigit(char c)
+{
+    return (c >= '0' && c <= '9');
+}
